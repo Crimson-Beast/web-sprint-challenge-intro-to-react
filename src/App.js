f8c228367b541c772bffeea28cc2a0809befcdd7
Daniel <dek.kneale@gmail.com>
2021-08-29 15:38:58 -0700

    mvp complete

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import axios from "axios";
+import React, { useState, useEffect} from "react";
+import Character from './components/Character'
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [characters, setCharacters] = useState([])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+      .then(({data}) => setCharacters(data))
+      .catch(err => console.log('Error getting data', err))
+  }, [])
 
-// codegrade test
-
+  
+  console.log(characters)
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+        characters.map(char => {
+          return <Character key={char.url} info={char}/>
+        })
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,36 @@
 // Write your Character component here
+import React from "react"
+import styled from 'styled-components'
+
+export default function Character({info}) {
+    return(
+        <NewCharacter className='character'>
+            <CharName>{info.name}</CharName>
+            <CharacterButton>{info.birth_year}</CharacterButton>
+            
+        </NewCharacter>
+    )}
+
+
+const NewCharacter = styled.div`
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        margin: 15px;
+        padding: 0 40px;
+        border: solid grey;
+        font-family: 'Bungee', cursive;
+
+`
+const CharacterButton = styled.button `
+        justify-content: center;
+        padding: 5px 35px;
+        margin: 10px;
+        font-weight: bold; 
+        
+`
+
+const CharName = styled.h2`
+    justify-content: center;
+    padding: 5px 35px;
+`
